02e0ebe6e3c596e4cd4458de90019f5059120f14
Brendan <bhiggins033@gmail.com>
2018-01-18 10:30:36 -0500

    Add sink to binary heap

--- js/binary_heap.js
+++ js/binary_heap.js
@@ -1,11 +1,18 @@
 class BinaryMaxHeap {
   constructor() {
-    this.pq = [];
+    this.pq = [null];
   }
 
-  insert(edge) {
+  put(edge) {
     this.pq.push(edge);
     this.bubble();
+  }
+
+  take() {
+    let max = this.pq[1];
+    this.pq[1] = this.pq.pop();
+    this.sink();
+    return max;
   }
 
   bubble() {
@@ -15,13 +22,37 @@ class BinaryMaxHeap {
       [this.pq[newIdx], this.pq[parentIdx]] =
         [this.pq[parentIdx], this.pq[newIdx]];
       newIdx = parentIdx;
+      if (newIdx === 1) { break; }
       parentIdx = Math.floor(newIdx/2);
     }
   }
 
-  remove() {
-    let max = this.pq.shift();
-    this.sink();
-    return max;
+  sink() {
+    let idx = 1;
+    let c1 = 2;
+    let c2 = 3;
+
+    while (this.pq[c1]) {
+      if (this.pq[c2]) {
+        if (this.pq[c2].weight > this.pq[c1].weight &&
+            this.pq[c2].weight > this.pq[idx].weight) {
+          [this.pq[c2], this.pq[idx]] = [this.pq[idx], this.pq[c2]];
+          idx = c2;
+        } else if (this.pq[c1].weight > this.pq[idx].weight) {
+          [this.pq[c1], this.pq[idx]] = [this.pq[idx], this.pq[c1]];
+          idx = c1;
+        } else {
+          break;
+        }
+      } else if (this.pq[c1].weight > this.pq[idx].weight) {
+        [this.pq[c1], this.pq[idx]] = [this.pq[idx], this.pq[c1]];
+        idx = c1;
+      } else {
+        break;
+      }
+
+      c1 = idx * 2;
+      c2 = idx * 2 + 1;
+    }
   }
 }
